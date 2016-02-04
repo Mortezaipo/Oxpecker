062a47887eb45fdaee9e91d8eb44e7542abee1a3
Mortezaipo <mortezaipo@gmail.com>
2016-02-04 12:29:25 +0330

    Update database class - according to use SQLAlchemy, implementing our class stoped.

--- src/database.py
+++ src/database.py
@@ -1,14 +1,13 @@
 from sqlalchemy import create_engine, MetaData, Table, sql
 from sqlalchemy.orm import sessionmaker
-import settings
-import schema
+from src import settings
 
 
 class Database(object):
-    """Databse class for all relations and commands"""
-    __engine = None
-    __connect = None
-    __session = None
+    """Database class for all relations and commands"""
+    _engine = None
+    _connect = None
+    _session = None
 
     def __new__(self):
         """Singletone implementation for database"""
@@ -18,43 +17,11 @@ class Database(object):
 
     def __init__(self):
         """Connecting to database"""
-        self.__engine = create_engine(settings.db_engine)
-        self.__connect = self.__engine.connect()
-        session = sessionmaker(bind=self.__engine)
-        self.__session = session()
+        self._engine = create_engine(settings.db_engine)
+        self._connect = self._engine.connect()
+        session = sessionmaker(bind=self._engine)
+        self._session = session()
 
-    def install(self):
-        """Installing tables on database
-
-        Returns:
-            True/False about tables creation status.
-        """
-        schema.Base.metadata.create_all(self.__engine)
-        return True
-
-    def insert(self, schema, data):
-        """Insert method for adding new data in database.
-
-        Args:
-            schema: schema class.
-            data: dictionary of data.
-        Returns:
-            Return True/False which related to status of insert action.
-        """
-        self.__session.add(schema(**data))
-        self.__session.commit()
-        return True
-
-    def delete(self, schema, where):
-        """Delete method for deleting data from database.
-
-        Args:
-            schema: schema class.
-            where: dictionary of determind map for remove.
-        Returns:
-            Return True/False which related to status of delete action.
-        """
-        rows = self.__session.query(schema).filter_by(**where).first()
-        self.__session.delete(rows)
-        self.__session.commit()
-        return True
+    @property
+    def session(self):
+        return self._session
