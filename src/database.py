c696943701d23b14dc7a91f7eda3534fb9947714
Mortezaipo <mortezaipo@gmail.com>
2016-02-08 21:48:34 +0330

    Update Database class - remove SQLAlchemy and use psycopg2 library instead.

--- src/database.py
+++ src/database.py
@@ -1,27 +1,111 @@
-from sqlalchemy import create_engine, MetaData, Table, sql
-from sqlalchemy.orm import sessionmaker
-from src import settings
+import psycopg2 as pgsql
+import psycopg2.extras as pgsql_extras
 
 
 class Database(object):
-    """Database class for all relations and commands"""
-    _engine = None
-    _connect = None
-    _session = None
+    """Database library for connecting to PostgreSQL database"""
 
-    def __new__(self):
-        """Singletone implementation for database"""
-        if not hasattr(self, 'instance'):
-            self.instance = super(Database, self).__new__(self)
-        return self.instance
+    _con = None
+    _cur = None
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Database, cls).__new__(cls)
+        return cls.instance
 
     def __init__(self):
-        """Connecting to database"""
-        self._engine = create_engine(settings.db_engine)
-        self._connect = self._engine.connect()
-        session = sessionmaker(bind=self._engine)
-        self._session = session()
+        if not self._con:
+            self.connect()
 
-    @property
-    def session(self):
-        return self._session
+    def connect(self):
+        """Connecting to database"""
+        try:
+            self._con = pgsql.connect(database="oxpecker", user="morteza", password="136", host="localhost")
+            self._cur = self._con.cursor(cursor_factory=pgsql_extras.DictCursor)
+        except pgsql.DatabaseError as err:
+            raise "Connection failed! Check connection settings. [%s]" % err
+
+    def disconnect(self):
+        """Closing database open connection"""
+        if self._con:
+            self._con.close()
+
+    def fetch_all(self, query, fetch_one=False):
+        """Fetch records on SQL query
+
+        :param query: SQL command.
+        :param fetch_one: fetching just 1 records.
+        :return: records of result which fetched from database.
+        """
+        if not self._con:
+            self.connect()
+
+        try:
+            self._cur.execute(query)
+            if fetch_one:
+                result = self._cur.fetchone()
+            else:
+                result = self._cur.fetchall()
+            return result
+        except pgsql.DatabaseError as err:
+            raise "Fetch all query failed! Check your SQL query. [%s]" % err
+
+    def delete(self, query):
+        """Delete SQL execution.
+
+        :param query: SQL command.
+        :return: True/False about action result.
+        """
+        if not self._con:
+            self.connect()
+
+        try:
+            self._cur.execute(query)
+            self._con.commit()
+        except pgsql.DatabaseError as err:
+            raise "Delete query failed! Check your SQL query. [%s]" % err
+
+    def insert(self, query):
+        """Insert SQL execution.
+
+        :param query: SQL command.
+        :return: True/False about action result.
+        """
+        if not self._con:
+            self.connect()
+
+        try:
+            self._cur.execute(query)
+            self._con.commit()
+        except pgsql.DatabaseError as err:
+            raise "Insert query failed! Check your SQL query. [%s]" % err
+
+    def update(self, query):
+        """Update SQL execution.
+
+        :param query: SQL command.
+        :return: True/False about action result.
+        """
+        if not self._con:
+            self.connect()
+
+        try:
+            self._cur.execute(query)
+            self._con.commit()
+        except pgsql.DatabaseError as err:
+            raise "Insert query failed! Check your SQL query. [%s]" % err
+
+    def execute(self, query):
+        """Execute SQL query.
+
+        :param query: SQL command.
+        :return: True/False about action result.
+        """
+        if not self._con:
+            self.connect()
+
+        try:
+            self._cur.execute(query)
+            self._con.commit()
+        except pgsql.DatabaseError as err:
+            raise "Execute query failed! Check your SQL query. [%s]" % err
