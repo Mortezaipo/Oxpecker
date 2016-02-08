848ce72f24822250ad0d83729f524a4def7df806
Mortezaipo <mortezaipo@gmail.com>
2016-02-08 21:51:30 +0330

    Add 'user' patterns in routes

--- src/routes.py
+++ src/routes.py
@@ -2,6 +2,7 @@ import tornado.web
 from src import users
 
 url = tornado.web.Application([
-    (r"/users/", users.UserInformation),
+    (r"/users/", users.User),
+    (r"/users/information/", users.UserInformation),
     (r"/users/authentication/", users.UserAuthentication),
 ])
