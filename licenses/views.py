5d1b75b6f99840b54c1cee1df03d8577c87753d8
Mortezaipo <mortezaipo@gmail.com>
2016-02-18 23:24:42 +0330

    Add 'index' part of license which show list of licenses in views

--- licenses/views.py
+++ licenses/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from models import License
+from forms import LicenseForm
 
-# Create your views here.
+
+def index(request):
+    data = {'licenses': License.objects.all()}
+    return render(request, 'licenses/index.html', data)
--- oxpecker/urls.py
+++ oxpecker/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+from licenses import views as license_views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^licenses/$', license_views.index, name="licenses_index"),
 ]
