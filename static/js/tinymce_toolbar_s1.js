d7b2a2d8efa09564f53848d62e5251217ed655c9
Mortezaipo <mortezaipo@gmail.com>
2016-02-19 21:12:51 +0330

    Update TinyMCE default toolbar and save in extra js file and use it in new.html file

---
+++ static/js/tinymce_toolbar_s1.js
@@ -0,0 +1,7 @@
+tinymce.init({
+   selector: "#richtext",
+   menubar: false,
+   statusbar: false,
+   plugins: 'textcolor table code link',
+   toolbar1: 'undo redo | fontselect | fontsizeselect | forecolor backcolor | bold italic underline | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | table | link | code'
+});
