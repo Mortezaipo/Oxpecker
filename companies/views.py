aced5e02fe97f82334b1ed53b50e63be40bfb32e
Mortezaipo <mortezaipo@gmail.com>
2016-02-26 01:51:16 +0330

    Update 'new' action in Company part - add validation label and classes in templates, move developer_form & company_form errors in errors.html, update base_s1 layout

--- companies/views.py
+++ companies/views.py
@@ -24,17 +24,17 @@ def new(request):
             company = company_form.save(commit=False)
             company.user = request.user
             company.save()
-            messages.add_message(request, messages.SUCCESS, "New company has been added successfully.")
             
             if developer_form.is_valid():
                 for dev in developer_form:
                     action = dev.save(commit=False)
                     action.company = company
                     action.save()
-                messages.add_message(request, messages.SUCCESS, "dev saved.")
+                messages.add_message(request, messages.SUCCESS, "New company with its team memebers have been saved successfully.")
+                return redirect(reverse('companies_index'))
             else:
-                messages.add_message(request, messages.ERROR, "dev not saved.")
-                
+                company.delete()
+                messages.add_message(request, messages.ERROR, "Team memebers saving failed. Check yout data.")
         else:
             messages.add_message(request, messages.ERROR, "Add new company failed. Check yout data.")
     data = {'company_form': company_form, 'developer_form': developer_form}
